27c7b0f5cb074d536540cb3d0c6bd228725da9fc
Boccanfuso Raphael <raphael.boccanfuso@gmail.com>
2021-05-17 15:28:16 +0200

    img

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -90,21 +90,7 @@ const IndexPage = (props) => {
 
   return (
     <Layout>
-      <SEO
-        title={site.title}
-        description={site.description}
-        keywords={site.keywords}
-      />
-      <Container>
-        <h1 hidden>Welcome to {site.title}</h1>
-        {postNodes && (
-          <BlogPostPreviewList
-            title="Latest blog posts"
-            nodes={postNodes}
-            browseMoreHref="/archive/"
-          />
-        )}
-      </Container>
+      <h1>premier titre</h1>
     </Layout>
   );
 };
