952e235f8b267704f64d48597762b5425eb7aa08
tiago-carvalhodev <82232884+tiago-carvalhodev@users.noreply.github.com>
2021-06-16 08:58:03 +0200

    fix

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -11,8 +11,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-postcss",
     "gatsby-plugin-react-helmet",
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: "gatsby-source-sanity",
       options: {
