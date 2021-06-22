5183055f1e81aa48d1bb88d7e5a06d5d95850daa
tiago-carvalhodev <82232884+tiago-carvalhodev@users.noreply.github.com>
2021-06-22 13:21:58 +0200

    page

--- web/src/components/layoutProduit.js
+++ web/src/components/layoutProduit.js
@@ -4,6 +4,7 @@ import Main from "./main";
 import "../styles/layout.css";
 import * as styles from "./layoutProduit.module.css";
 import Footer from "./footer";
+
 const LayoutProduit = ({ children, show, nav, showNav, siteTitle }) => (
   <>
     <div className={styles.page}>
--- web/src/templates/produit-page.js
+++ web/src/templates/produit-page.js
@@ -1,11 +1,17 @@
 import { graphql } from "gatsby";
-import React from "react";
+import React, {useState} from "react";
 import GraphQLErrorList from "../components/graphql-error-list";
 import LayoutProduit from "../components/layoutProduit";
 import Container from "../components/container";
 import SEO from "../components/seo";
 import { toPlainText } from "../lib/helpers";
 import ProduitPage from "../components/produit-page";
+import Header from "../components/header";
+import { BsArrowLeft } from "react-icons/bs";
+import { Link } from "gatsby";
+import PortableText from "../components/portableText";
+
+import * as styles from "../components/produit.module.css";
 
 export const query = graphql`
   query ProduitTemplateQuery($id: String!) {
@@ -41,8 +47,15 @@ export const query = graphql`
 const ProduitPageTemplate = (props) => {
   const { data, errors } = props;
   const post = data && data.post;
+
+  const [nav, showNav] = useState(false);
+
+const [show, showShow] = useState(false);
   return (
-    <LayoutProduit>
+    <React.Fragment>
+    <div className={styles.hh}>
+      <Header showNav={showNav} nav={nav} show={show}/>
+    </div>
       {errors && <SEO title="GraphQL Error" />}
       {post && (
         <SEO
@@ -57,9 +70,24 @@ const ProduitPageTemplate = (props) => {
           <GraphQLErrorList errors={errors} />
         </Container>
       )}
-
-      {post && <ProduitPage {...post} />}
-    </LayoutProduit>
+      <div className={styles.con}>
+        <div className={styles.wrapperProduit}>
+        <Link to={"/game/" + `${post.game.slug.current}`} className={styles.retour}>
+          <BsArrowLeft className={styles.arrow} />
+          <div className={styles.back}><p>RETOUR</p></div>
+        </Link>
+        <img src={post.image.asset.url} alt="" className={styles.bouteille} />
+        <div className={styles.game}>
+          <h1> {post.game.title} </h1>
+          <img src={post.game.logo.asset.url} alt="" className={styles.logo} />
+          <div className={styles.desc}>
+            <PortableText blocks={post.fiche._rawFr} />
+          </div>
+        </div>
+        <img src={post.game.logo.asset.url} alt="" className={styles.logo2} />
+      </div>
+    </div>
+    </React.Fragment>
   );
 };
 
