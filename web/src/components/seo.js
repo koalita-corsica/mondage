df7127c2bec0f88fef6d1b13ee68cf385f88776f
tiago-carvalhodev <82232884+tiago-carvalhodev@users.noreply.github.com>
2021-06-29 11:37:22 +0200

    YAAAA

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -57,6 +57,10 @@ function SEO({ description, lang, meta, keywords, title, image }) {
           name: "twitter:description",
           content: metaDescription,
         },
+        {
+          name: "Cache-control",
+          content: "max-age=31536000"
+        }
       ]
         .concat(
           keywords && keywords.length > 0
