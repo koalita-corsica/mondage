435c4cbbbb71ddd6f4ba9cec73c627ac0dee6390
tiago-carvalhodev <82232884+tiago-carvalhodev@users.noreply.github.com>
2021-06-29 09:07:37 +0200

    WOW

--- web/src/containers/layout.js
+++ web/src/containers/layout.js
@@ -1,7 +1,6 @@
 import { graphql, StaticQuery } from "gatsby";
 import React, { useState } from "react";
 import Layout from "../components/layout";
-import { Helmet } from "react-helmet";
 
 const query = graphql`
   query SiteTitleQuery {
@@ -27,9 +26,6 @@ function LayoutContainer(props) {
         }
         return (
           <React.Fragment>
-            <Helmet>
-              <meta name="Cache-control" content="max-age=31536000" />
-            </Helmet> 
             <Layout
               {...props}
               showNav={showNav}
