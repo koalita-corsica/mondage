83f787ac11d7e29c5bc910242186ea9058d96671
Tiago <tiago@koalita.corsica>
2021-09-07 09:56:56 +0200

    fonts

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -113,7 +113,11 @@ const Header = ({ nav, showNav, show }) => (
   <title> Mondange </title>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="author" content="Koalità" />
-  <link rel="preload" as="font" />
+  <link rel="preload" href="../fonts/Reislust-97D7.ttf" as="font" crossorigin="anonymous"/>
+  <link rel="preload" href="../fonts/FuturaPTBook.otf" as="font" crossorigin="anonymous"/>
+  <link rel="preload" href="../fonts/FuturaPTMedium.otf" as="font" crossorigin="anonymous"/>
+  <link rel="preload" href="../fonts/FuturaPTBold.otf" as="font" crossorigin="anonymous"/>
+  <link rel="preload" href="../fonts/FuturaPTBookOblique.otf" as="font" crossorigin="anonymous"/>
   <link rel="preconnect" href="https://cdn.ampproject.org/v0.js" />
 </Helmet>
     <div className={styles.siteTitle}>
