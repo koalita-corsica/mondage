7f43c2d373c7b4daea1f6028457f05b4e592322d
Tiago <tiago@koalita.corsica>
2021-09-07 13:15:49 +0200

    yo

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -9,6 +9,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   plugins: [
+    'gatsby-plugin-preact',
     "gatsby-plugin-postcss",
     "gatsby-plugin-react-helmet",
     {
@@ -30,11 +31,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
-        printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        printRejected: true,
       },
     },
   ],
