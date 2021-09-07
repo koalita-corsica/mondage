3f4cb6ca60ede60e5d3c2c76939cbb343edc01a6
Tiago <tiago@koalita.corsica>
2021-09-07 09:13:56 +0200

    fix

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
   resolve: `gatsby-plugin-offline`,
   options: {
-    precachePages: [`/galerie/`, `index`, `/presse/`],
+    precachePages: [`/galerie/`, `/`, `/presse/`],
   },
 },
   ],
