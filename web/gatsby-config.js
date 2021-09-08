c05062566733d4a689188203faa63baf3a7acb82
Tiago <tiago@koalita.corsica>
2021-09-08 14:50:19 +0200

    yay

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
     {
   resolve: `gatsby-plugin-offline`,
   options: {
-    precachePages: [`/`, `/vins`, `/galerie`, `/domaine`, `/presse`, `/contact`],
+    precachePages: [`/`, `/galerie`, `/domaine`,],
   },
 },
   ],
